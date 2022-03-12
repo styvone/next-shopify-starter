98ac37ebe0ac7e1c4c1b6683b7c601de34a61c97
Steven La <stevenla@Stevens-MBP.cable.rcn.com>
2022-03-12 14:36:23 -0500

    finish marquee and instagram reel

--- components/GentleAndRestorative.js
+++ components/GentleAndRestorative.js
@@ -1,7 +1,10 @@
+import Marquee from "react-fast-marquee";
+
 function GentleAndRestorative() {
     return (
         <div style={{backgroundColor: '#283F91', color: '#EBE5DB'}} className="h-28 w-full flex flex-col justify-center">
-            <div className="h-14 w-full overflow-hidden whitespace-nowrap marquee">
+            <div className="h-14 w-full overflow-hidden whitespace-nowrap">
+            <Marquee gradient={false} speed={50}>
                 <h1 className="ivyPrestoThin text-5xl inline mr-28">
                     <em>Gentle</em> and <em>restorative</em>
                     <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
@@ -16,81 +19,7 @@ function GentleAndRestorative() {
                     <em>Gentle</em> and <em>restorative</em>
                     <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
                 </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>   
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>   
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>
-
-                <h1 className="ivyPrestoThin text-5xl inline mr-28">
-                    <em>Gentle</em> and <em>restorative</em>
-                    <img alt="pill" className="w-14 h-14 inline object-contain ml-28" src="/pill.svg" />
-                </h1>        
+            </Marquee>  
             </div>
         </div>
     )
--- components/InstagramReel.js
+++ components/InstagramReel.js
@@ -1,9 +1,57 @@
+import Link from 'next/link';
+import Marquee from "react-fast-marquee";
+
+function imageWrapper(src, href) {
+  return (
+    <div className="h-48 w-48 overflow-hidden inline-block mr-8 rounded-lg">
+      <Link
+        href={href}
+        passHref
+      >
+        <img alt="logo" className="h-56 w-56 inline transform duration-500 ease-in-out hover:scale-110 cursor-pointer" src={src} />
+      </Link>
+    </div>
+  );
+}
+
+function createImageReel() {
+  return (
+    <Marquee gradient={false} speed={50}>
+      <div className="whitespace-nowrap overflow-hidden">
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+        {imageWrapper('/images/instagram/test.png', '/instagram')}
+      </div>
+    </Marquee>
+  );
+}
+
 function InstagramReel() {
   return (
-    <div style={{backgroundColor: "#EBE5DB"}} className="h-60 w-full flex">
-
+      <div style={{backgroundColor: "#EBE5DB"}} className="h-80 w-full flex flex-col justify-evenly">
+        <div className="h-8 flex justify-end">
+          <div style={{color: '#283F91'}} className="flex flex-col justify-center">
+            <Link
+                href="/instagram"
+                passHref
+              >
+                <a className="coreSans text-l pr-12">
+                <u>FOLLOW US ON INSTAGRAM +</u>
+                </a>
+            </Link>
+          </div>
+        </div>
+        <div className="h-56 flex flex-col justify-center">
+          {createImageReel()}
+        </div>
       </div>
-  )
-}
+    );
+  }
 
 export default InstagramReel
