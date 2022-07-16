aeeaadc10bf29cd38cfeb0beecb03c57ee8e3f00
Steven La <stevenla@Stevens-MBP.lan>
2022-07-15 21:32:25 -0400

    site map

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
   env: {
     siteTitle: 'Boquet Vaginal Prebiotic',
     siteDescription: 'An easy-to-use vaginal tablet that relieves irritation and odor changes by promoting the growth of good bacteria in the vaginal flora (instead of stripping it away). Backed by science, our tablet is a natural prebiotic that is gentle enough to be used everyday.',
-    siteKeywords: 'vaginal tablet, vaginal flora, vagina prebiotic, prebiotic, health, good bacteria, natural, natural prebiotic, balance, hormonal balance',
+    siteKeywords: 'prebiotic, health, vaginal tablet, vaginal flora, vagina prebiotic, good bacteria, natural, natural prebiotic, balance, hormonal balance, restore, fishy odor, vaginal odor, bacterial vaginosis',
     siteUrl: 'https://www.withboquet.com/',
     siteImagePreviewUrl: '/images/main.jpg',
     instagramHandle: '@withboquet',
